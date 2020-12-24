2e835f5535c748be74427a68188a30f17fd6fa53
dygksquf5 <66229916+dygksquf5@users.noreply.github.com>
2020-12-24 15:36:09 +0900

    update

--- Algorithm_python/소수찾기.py
+++ Algorithm_python/소수찾기.py
@@ -56,5 +56,25 @@ def solution(numbers):
 #     return len(second)
 
 
+
+# 다른사람 풀이 ! set 을 잘 활용한 풀이닷! 참고하기 @!!
+# from itertools import permutations
+#
+#
+# def solution(n):
+#     a = set()
+#     for i in range(len(n)):
+#         # 단순 a 를 replace 하는게 아닌, |= 합집합으로 a 에 중복된거 없이 계속 추가할 수 있게 하기. 쩌넹
+#         a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+#
+#     a -= set(range(0, 2))  # a 에 쌓여있는 애들중에 소수는 결국 0과 1 제외해야 되니까, set으로
+#     # range 0~1 까지 수의 차집합 구하는 방식으로 제외시켜버리기.
+#     print(int(max(a)))
+#     for i in range(2, int(max(a) ** 0.5) + 1):
+#         a -= set(range(i * 2, max(a) + 1, i))
+#     return len(a)
+
+
+
 print(solution("17"))
 print(solution("011"))
