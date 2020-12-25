72762a339c3759bbc064adb29c4db34f3f824a88
dygksquf5 <66229916+dygksquf5@users.noreply.github.com>
2020-12-25 23:14:56 +0900

    ㅎㅎㅎ merry christmas !

--- Algorithm_python/메리크리스마스!!:).py
+++ Algorithm_python/메리크리스마스!!:).py
@@ -1,21 +1,19 @@
 from colorama import init
-
 init()
-
 from colorama import Fore, Back, Style
 
 size = 11
 
 for i in range(0, size):
     if i == 0:
-        print(Fore.YELLOW + '*'.center(size, ' '))
+        print(Fore.YELLOW + '💚'.center(size, ' '))
     elif i % 2 == 0 and i > 0:
         print(Fore.GREEN + ('*' * i).center(size, ' '))
     else:
         print(Fore.RED + ("'" * (i+1)).center(size, " "))
 
 init(autoreset=True)
-print("")
+print(" ")
 print(Back.RED + "Merry".center(size + 2, ' '))
 print(Back.BLUE + "Christmas!".center(size+2, ' '))
 print(" ")
