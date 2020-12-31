0902c6e6bbfa2cbde06980efe9be23d511700102
dygksquf5 <66229916+dygksquf5@users.noreply.github.com>
2020-12-31 12:17:49 +0900

    new_mini_Project

---
+++ python_mini_Project/Virtual_Assistant/main.py
@@ -0,0 +1,73 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_command():
+    try:
+        with sr.Microphone() as source:
+            print(" listening ... ")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'bixby' in command:
+                command = command.replace('bixby', '')
+                print(command)
+
+    except:
+        pass
+    return command
+
+
+def run_bixby():
+    command = get_command()
+    # print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk("Current time is "+time)
+    elif 'where is' in command:
+        info = command.replace('where is', '')
+        finding = wikipedia.summary(info, 1)
+        print(finding)
+        talk(finding)
+    elif 'what is' in command:
+        info = command.replace('where is', '')
+        finding = wikipedia.summary(info, 1)
+        print(finding)
+        talk(finding)
+    elif 'who is' in command:
+        info = command.replace('where is', '')
+        finding = wikipedia.summary(info, 1)
+        print(finding)
+        talk(finding)
+    elif 'find ' in command:
+        info = command.replace('find', '')
+
+        finding = pywhatkit.search(info)
+        print(finding)
+        talk(finding)
+
+    else:
+        talk("please say the command again")
+
+
+while True:
+    run_bixby()
