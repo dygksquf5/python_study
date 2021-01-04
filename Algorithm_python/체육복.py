5a4dd932c4a7e7105d9588ceb247c0463eb552ca
dygksquf5 <66229916+dygksquf5@users.noreply.github.com>
2021-01-04 20:07:43 +0900

    solve

--- Algorithm_python/체육복.py
+++ Algorithm_python/체육복.py
@@ -3,17 +3,42 @@
 
 import collections
 
+
+# def solution(n, lost, reserve):
+#     for r in reserve[:]:
+#         if r in lost:
+#             lost.remove(r)
+#             reserve.remove(r)
+#
+#     for i in reserve[:]:
+#         if i + 1 in lost:
+#             lost.remove(i + 1)
+#             continue
+#         if i - 1 in lost:
+#             lost.remove(i - 1)
+#             continue
+#     return n - len(lost)
+
+
+# 5번은 중복되는 경우를 따로 다시 for 문으로 빼줘서 해결
+
+# 11번이 안된다.. 뭐때문일까
+
+
 def solution(n, lost, reserve):
-    for i in reserve:
-        if i in lost:
-            lost.remove(i)
+    for r in reserve[:]:
+        if r in lost:
+            lost.remove(r)
+            reserve.remove(r)
+
+    for i in reserve[:]:
+        if i - 1 in lost:
+            lost.remove(i - 1)
             continue
         if i + 1 in lost:
-            lost.remove(i+1)
-        elif i - 1 in lost:
-            lost.remove(i-1)
+            lost.remove(i + 1)
+            continue
     return n - len(lost)
 
-
- # 5번과 11번이 안된다.. 뭐때문일까
- 
+# 이걸로 해결함!!!
+# 빼기를 먼저 해서 왼쪽에 있는 애 먼저 해결하고, -1 이 없는경우에 +1 을 한 오른쪽 있는 애를 해결하는 식으로 해야 테스트 케이스 해결 가능 
